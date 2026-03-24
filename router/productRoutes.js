9ca8d94c10349f8c89ed1aff1d48a12b626c8668
unknown <LokeshDhinakran@gmail.com>
2026-03-24 20:52:40 +0530

    just updayion on controllers

--- router/productRoutes.js
+++ router/productRoutes.js
@@ -4,13 +4,10 @@ const router = express.Router();
 const upload = require("../middleware/upload");
 const productController = require("../controller/productController");
 
-// 🔥 GET
 router.get("/", productController.getAllProducts);
 
-// 🔥 GET BY ID
 router.get("/products/:id", productController.getProductById);
 
-// 🔥 POST
 router.post(
   "/products",
   upload.fields([
